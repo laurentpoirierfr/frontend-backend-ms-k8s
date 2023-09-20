9238deb32be28c7405c6f3c49f538bf1de658e84
Laurent Poirier <laurent.poirier@ima.eu>
2023-09-20 15:51:11 +0200

    refacto frontend

--- app/frontend-app/src/index.js
+++ app/frontend-app/src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './app/App';
-import * as serviceWorker from './serviceWorker';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { ChakraProvider } from '@chakra-ui/react';
 
-ReactDOM.render(
-  <BrowserRouter basename="/">
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <ChakraProvider>
     <App />
-  </BrowserRouter>
-, document.getElementById('root'));
+  </ChakraProvider>
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();

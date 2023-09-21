5cbb243a569b62299f09b98d7c3c3d85b3ef6e2a
Laurent Poirier <laurent.poirier@ima.eu>
2023-09-21 09:43:01 +0200

    change frontend

--- app/frontend-app/src/App.js
+++ app/frontend-app/src/App.js
@@ -1,22 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './Components/Navbar';
-import About from './Pages/About';
-import Login from './Pages/Login';
-import Home from './Pages/Home';
-import Skills from './Pages/Skiils';
-import Projects from './Pages/Projects';
 
+import './components/Menu';
+import Menu from './components/Menu';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Home/>
-      <About />
-      <Skills />
-      <Projects/>
-      <Login/>
+      <Menu />
+
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }

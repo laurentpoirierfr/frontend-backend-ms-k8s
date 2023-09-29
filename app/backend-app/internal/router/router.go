7067b0b9abba18e398d766bde045a9a690d00e86
Laurent Poirier <laurent.poirier@ima.eu>
2023-09-29 12:58:54 +0200

    add prometheus

--- app/backend-app/internal/router/router.go
+++ app/backend-app/internal/router/router.go
@@ -11,9 +11,29 @@ import (
 	docs "github.com/laurentpoirierfr/bff/api"
 	swaggerfiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
+
+	"github.com/penglongli/gin-metrics/ginmetrics"
 )
 
 func NewRouter(context context.Context, config util.Config, app *gin.Engine) {
+
+	// ==========================================================================================
+	// Monitoring Prometheus
+	// ==========================================================================================
+	m := ginmetrics.GetMonitor()
+	// +optional set metric path, default /debug/metrics
+	m.SetMetricPath("/metrics")
+	// +optional set slow time, default 5s
+	m.SetSlowTime(10)
+	// +optional set request duration, default {0.1, 0.3, 1.2, 5, 10}
+	// used to p95, p99
+	m.SetDuration([]float64{0.1, 0.3, 1.2, 5, 10})
+
+	// set middleware for gin
+	m.Use(app)
+	// ==========================================================================================
+	// Swagger
+	// ==========================================================================================
 	app.GET("/swagger/*any", func(context *gin.Context) {
 		docs.SwaggerInfo.Host = context.Request.Host
 		ginSwagger.WrapHandler(swaggerfiles.Handler)(context)
@@ -24,11 +44,18 @@ func NewRouter(context context.Context, config util.Config, app *gin.Engine) {
 	})
 
 	indexController := controller.NewIndexController(context, config)
+
+	// ==========================================================================================
+	// API
+	// ==========================================================================================
 	api := app.Group("/api")
 	{
 		api.GET("/menus", indexController.Menus)
 	}
 
+	// ==========================================================================================
+	// OPS
+	// ==========================================================================================
 	opsCtrl := controller.NewOpsController(context, config)
 
 	ops := app.Group("/ops")

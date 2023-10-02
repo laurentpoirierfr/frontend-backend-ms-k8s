0df9d2478c02c26c2b94f71e270befebc5d29217
Laurent Poirier <laurent.poirier@ima.eu>
2023-10-02 09:35:32 +0200

    change metrics route

--- app/backend-app/internal/router/router.go
+++ app/backend-app/internal/router/router.go
@@ -22,7 +22,7 @@ func NewRouter(context context.Context, config util.Config, app *gin.Engine) {
 	// ==========================================================================================
 	m := ginmetrics.GetMonitor()
 	// +optional set metric path, default /debug/metrics
-	m.SetMetricPath("/metrics")
+	m.SetMetricPath("/ops/metrics")
 	// +optional set slow time, default 5s
 	m.SetSlowTime(10)
 	// +optional set request duration, default {0.1, 0.3, 1.2, 5, 10}
